1c3d9542a55a5504b4a75b590d795cdb1a51896d
Andres Valle <andresvalle.isi@gmail.com>
2025-04-24 01:31:28 -0300

    wip: migrations

---
+++ app/seeds/admin.py
@@ -0,0 +1,15 @@
+from app.extensions import db
+from app.models import Admin
+from app import create_app
+from werkzeug.security import generate_password_hash
+
+def create_default_admin():
+    app = create_app()
+    with app.app_context():
+        if not Admin.query.filter_by(username="admin").first():
+            admin = Admin(username="admin", password=generate_password_hash("admin"))
+            db.session.add(admin)
+            db.session.commit()
+            print("[SEED] Default admin user created.")
+        else:
+            print("[SEED] Admin user already exists.")
